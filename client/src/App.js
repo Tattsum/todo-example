474e74d624f56bbd383cc44a305b0bae9a622a7c
Tatsuma.kano <tatsuma.kano@yappli.co.jp>
2023-08-20 20:13:01 +0900

    feat: add create buttom

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+import client from './apolloClient';
+import TodoList from './TodoList';
+import CreateTodo from './CreateTodo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>My Todos</h1>
+        <CreateTodo />
+        <TodoList />
+      </div>
+    </ApolloProvider>
   );
 }
 
 export default App;
+
